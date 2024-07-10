cfaadfca1678b1a263cea94b15a47b436ae4e0dd
zah-ara <161930119+zah-ara@users.noreply.github.com>
2024-07-10 14:29:28 +0500

    first commit

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 #! /usr/bin/env node
-import inquirer from 'inquirer';
-const answers = await inquirer.prompt([
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const inquirer_1 = __importDefault(require("inquirer"));
+const answers = await inquirer_1.default.prompt([
     {
         name: "sentence",
         type: "input",
